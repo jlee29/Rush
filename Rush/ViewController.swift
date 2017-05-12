3998fef7d7dad3a22ee5e308401339693f699e93
jlee29 <jlee29@stanford.edu>
2017-05-12 13:49:21 -0700

    added basic fb login

--- Rush/ViewController.swift
+++ Rush/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y:view.frame.height - 75, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
+        
+        loginButton.readPermissions = ["email"]
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logged out")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if (error != nil) {
+            print(error)
+        }
+        print("worked")
     }
 
     override func didReceiveMemoryWarning() {
