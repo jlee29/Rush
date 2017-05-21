d395ae474de62d4bb07351496da3786eac9c3c97
Peter Lu <6peterlu@gmail.com>
2017-05-20 19:00:23 -0700

    firebase push + encoder

--- Rush/RushModel.swift
+++ Rush/RushModel.swift
@@ -7,11 +7,27 @@
 //
 
 import Foundation
+import Firebase
 
 struct RushModel {
     func submitToDatabase(with location: String, time: String, price: Double) {
         print(location)
         print(time)
         print(price)
+        let ref = FIRDatabase.database().reference()
+        let defaults = UserDefaults.standard
+        let name = defaults.string(forKey: "realName")
+        let email = defaults.string(forKey: "email")
+        ref.child(encodeFirebaseKey(inputStr: email!)).setValue(name)
+    }
+    func encodeFirebaseKey(inputStr: String) -> String {
+        var newStr = inputStr
+        newStr = inputStr.replacingOccurrences(of: ".", with: "😍")
+        newStr = newStr.replacingOccurrences(of: "$", with: "🙄")
+        newStr = newStr.replacingOccurrences(of: "[", with: "🙈")
+        newStr = newStr.replacingOccurrences(of: "]", with: "😉")
+        newStr = newStr.replacingOccurrences(of: "#", with: "😒")
+        newStr = newStr.replacingOccurrences(of: "/", with: "😂")
+        return newStr
     }
 }
